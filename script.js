615614086874f6005d896dfb751ef0572cc43094
Swapnil Dhamal <51965205+swapnil7895@users.noreply.github.com>
2025-02-22 23:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('surpriseBtn').addEventListener('click', function() {
         alert('🎉 Parat ekda... Happyy birthday Sayuuu🎂🎁 Enjoy your day! I hope next year ekdm best jaoo.. Tula je je pahije te te bheto..🎂❤️');
     });
+    // Ensure the song plays only once
+    audio.addEventListener("ended", function() {
+        audio.pause();
+    });
 });
