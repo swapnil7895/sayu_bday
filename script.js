7462d948ee13b3e7c5d240e44d487388d9ff0993
Swapnil Dhamal <51965205+swapnil7895@users.noreply.github.com>
2025-02-22 23:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.getElementById('surpriseBtn').addEventListener('click', function() {
-    alert('🎉 Surprise! Wishing you a fantastic year ahead! 🎂🎁');
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = document.getElementById("birthdaySong");
+    audio.play().catch(error => {
+        console.log("Autoplay failed: ", error);
+    });
 });
 
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+    alert('🎉 Parat ekda... Happyy birthday Sayuuu🎂🎁 Enjoy your day! I hope next year ekdm best jaoo.. Tula je je pahije te te bheto..🎂❤️');
+});
